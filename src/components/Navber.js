ea1f3e23af27973144dda2605c1dac5e8976b90d
Shanto <rakibulhasansanto25@gmail.com>
2021-04-03 13:38:16 +0600

    Finish navbar section

--- src/components/Navber.js
+++ src/components/Navber.js
@@ -21,35 +21,72 @@ const NavbarWrap = styled.div`
   justify-content: space-between;
 `;
 const Logo = styled.div`
- 
+    width: 120px;
+    height: 22px;
+    background: url(${Img}) 0 45% no-repeat;
+    background-size: 100% 100%;
 `;
 
 const Nav = styled.nav`
-  
+flex: 1;
+position: relative;
+padding-left: 50px;
 `;
 
 const NavLink = styled(Link)`
-  
+  color: #000;
+  padding: 15px;
+  font-size: 16px;
+  line-height: 80px;
+  font-weight: 700;
+  text-decoration: none;
 `;
 
 const Search = styled.div`
-  
+  right: 235px;
+  position: absolute;
 `;
 
 const SearchWrap = styled.div`
- 
+ width: 220px;
+ height: 36px;
+ position: relative;
+ box-sizing: border-box;
+ background: rgba(179, 191, 201, 0.15);
+ line-height: 33px;
+ padding-left: 28px;
+ border-radius: 19px;
 `;
 
 const Input = styled.input`
-  
+color: #000;
+width: 196px;
+border: transparent;
+font-size: 12px;
+background: transparent;
+outline: none;
+&::placeholder{
+    color: #d1d8de;
+}
 `;
 
 const ButtonContainer = styled.div`
-  
+display: flex;
+align-item: center;
+justify-content: flex-end;
 `;
 
 const Button = styled.div`
- 
+width: 110px;
+color: #fff;
+cursor: pointer;
+height: 36px;
+font-size: 16px;
+box-sizing: border-box;
+background:#007cff;
+text-align: center;
+line-height: 36px;
+border-radius: 20px;
 `;
 
 const Navbar = () => {
@@ -71,7 +108,7 @@ const Navbar = () => {
           </SearchWrap>
         </Search>
         <ButtonContainer>
-          <Button>
+          <Button css={'color: #037cff; background: #fff;'}>
             SIGN UP
           </Button>
           <Button>
