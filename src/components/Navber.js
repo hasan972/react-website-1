c30ef9e031dc441f0b1129a3d50c3123414c4001
Shanto <rakibulhasansanto25@gmail.com>
2021-04-03 12:46:31 +0600

    Set the navbar css section

--- src/components/Navber.js
+++ src/components/Navber.js
@@ -1,11 +1,86 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components/macro';
+import Img from '../assects/logo.svg';
+import { Link } from 'react-router-dom';
 
-function Navber() {
-    return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
-    )
-}
+const NavbarContainer = styled.div`
+  width: 100%;
+  height: 80px;
+  z-index: 20;
+  background: #fff;
+`;
 
-export default Navber
+const NavbarWrap = styled.div`
+  width: 1200px;
+  height: 100%;
+  margin: 0 auto;
+  z-index: 20;
+  display: flex;
+  position: relative;
+  align-items: center;
+  justify-content: space-between;
+`;
+const Logo = styled.div`
+ 
+`;
+
+const Nav = styled.nav`
+  
+`;
+
+const NavLink = styled(Link)`
+  
+`;
+
+const Search = styled.div`
+  
+`;
+
+const SearchWrap = styled.div`
+ 
+`;
+
+const Input = styled.input`
+  
+`;
+
+const ButtonContainer = styled.div`
+  
+`;
+
+const Button = styled.div`
+ 
+`;
+
+const Navbar = () => {
+  return (
+    <NavbarContainer>
+      <NavbarWrap>
+        <Logo />
+        <Nav>
+          <NavLink to='/'>HOME</NavLink>
+          <NavLink to='/'>MARKET</NavLink>
+          <NavLink to='/'>TRADE</NavLink>
+          <NavLink to='/'>PRICING</NavLink>
+          <NavLink to='/'>DOWNLOAD</NavLink>
+          <NavLink to='/'>HELP</NavLink>
+        </Nav>
+        <Search>
+          <SearchWrap>
+            <Input type='text' placeholder='Symbol/Name' />
+          </SearchWrap>
+        </Search>
+        <ButtonContainer>
+          <Button>
+            SIGN UP
+          </Button>
+          <Button>
+              LOG IN
+          </Button>
+        </ButtonContainer>
+      </NavbarWrap>
+    </NavbarContainer>
+  );
+};
+
+export default Navbar;
